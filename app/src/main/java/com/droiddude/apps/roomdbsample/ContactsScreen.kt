15a8ab10603591bac3aa60a67ac653da78a02595
sakthi811 <sakthivel811@gmail.com>
2024-01-03 15:37:00 +0530

    Added Top Bar

--- app/src/main/java/com/droiddude/apps/roomdbsample/ContactsScreen.kt
+++ app/src/main/java/com/droiddude/apps/roomdbsample/ContactsScreen.kt
@@ -4,11 +4,13 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.droiddude.apps.roomdbsample.interfaces.ContactEvent
@@ -40,6 +43,11 @@ fun ContactsScreen(
     onEvent : (ContactEvent) -> Unit
 ) {
     Scaffold(
+        topBar = {
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                Text(text = "MyContacts", textAlign = TextAlign.Center, fontSize = 32.sp)
+            }
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 onEvent(ContactEvent.ShowDialog)
@@ -50,13 +58,13 @@ fun ContactsScreen(
                 )
             }
         }
-    ) { _ ->
+    ) { padding ->
         if(state.isAddingContact) {
             AddContactDialog(state = state, onEvent = onEvent)
         }
         
         LazyColumn(contentPadding = PaddingValues(16.dp),
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.fillMaxSize().padding(top = 50.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
             item {
